2fb11de0afdb3c082f4d2e9171100d5ef6ca0710
kosovic-creator <drasko.kosovic@icloud.com>
2025-08-25 07:16:25 +0200

    stanj3

--- app/stanje3/page.tsx
+++ app/stanje3/page.tsx
@@ -1,21 +1,53 @@
 'use client'
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
+
+type Stanje = {
+  id: number;
+  korisnik: string;
+  matičniBroj: string;
+  stanje: number;
+  kredit: number;
+  isplata: number;
+  uplate: number;
+};
 
 const Page = () => {
   const [kredit, setKredit] = useState<number | null>(null)
-  const [stanje, setStanje] = useState<number | null>(null)
+  const [stanje, setStanje] = useState<Stanje | null>(null)
   const [isActive, setIsActive] = useState<boolean | null>(null)
   const [uplata, setUplata] = useState<number | null>(null)
   const [isplata, setIsplata] = useState<number | null>(null)
 
+   useEffect(() => {
+      fetch('/api/stanje')
+        .then(async res => {
+          if (!res.ok) return null;
+          try {
+            return await res.json();
+          } catch {
+            return null;
+          }
+        })
+        .then(setStanje);
+    }, []);
+
   return (
     <>
       <button type='button'  className="flex-1 bg-green-500 hover:bg-green-450 text-white py-2 rounded font-medium transition cursor-pointer"  onClick={() => setUplata(500)}>Uplata</button>
-      <div>Kredit: {kredit}</div>
-      <div>Stanje: {stanje}</div>
+     
+      <div>
+        
+        <div>ID: {stanje?.id}</div>
+        <div>Korisnik: {stanje?.korisnik}</div>
+        <div>Matični broj: {stanje?.matičniBroj}</div>
+        <div>Iznos: {stanje?.stanje}</div>
+        <div>Kredit: {stanje?.kredit}</div>
+        <div>Isplata: {stanje?.isplata}</div>
+        <div>Uplate: {stanje?.uplate}</div>
+      </div>
       <div>Is Active: {isActive ? 'Yes' : 'No'}</div>
       <div>Uplata: {uplata}</div>
-      <div>Isplata: {isplata}</div>
+     
     </>
   )
 }
